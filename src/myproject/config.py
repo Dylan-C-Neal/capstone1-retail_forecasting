c824978619f805bc669473d2752f6283bdf0ec1b
Dylan Neal <Dylan@Dylans-MacBook-Pro.local>
2020-05-13 21:31:41 -0600

    Initial commit after setting up project directory

---
+++ src/myproject/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 21:08:02 2020
+
+@author: Dylan
+"""
+
+
+from pathlib import Path
+import numpy
+import pandas as pd
+
+data_dir_raw = Path('../data/raw')
---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 21:07:02 2020
+
+@author: Dylan
+"""
+
+
+from setuptools import setup, find_packages
+setup(name="capstone1-retail_forecasting", version="0.1")
